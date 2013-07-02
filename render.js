efc5db93b46ddbf9380c987e083af0716993252f
David <david.nonamedguy@gmail.com>
2013-07-02 21:27:01 +0100

    Some changes to the renderer

--- render.js
+++ render.js
@@ -3,19 +3,21 @@ var Handlebars = require("handlebars");
 
 // Check for filename
 if (process.argv.length < 3) {
-    console.log("Usage: node " + process.argv[1] + " Filename");
+    console.log("Usage: node " + process.argv[1] + " mustache_file data_file");
     process.exit(1);
 }
 
 var fs = require("fs"),
-    template = process.argv[2];
+    template = process.argv[2],
+    data_file = process.argv[3];
+    
 
-fs.readFile(template + ".mustache", 'utf8', function (err, data) {
+fs.readFile(template, 'utf8', function (err, data) {
     if (err) throw err;
 
     var compiled = Handlebars.compile(data);
 
-    fs.readFile(template + ".yaml", 'utf8', function (err, data) {
+    fs.readFile(data_file, 'utf8', function (err, data) {
         if (err) throw err;
 
         var input = require("js-yaml").load(data);
